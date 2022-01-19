f162399fc325a4d83e797e46fff2579c34d76d12
liz <lizz@1391.com>
2022-01-19 13:28:45 +0800

    feat:init rust demo

---
+++ hello-world/target/debug/deps/hello_world-5c7f155aaaae12aa.d
@@ -0,0 +1,5 @@
+/Users/yj/rustWork/rust-learning/hello-world/target/debug/deps/hello_world-5c7f155aaaae12aa: src/main.rs
+
+/Users/yj/rustWork/rust-learning/hello-world/target/debug/deps/hello_world-5c7f155aaaae12aa.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/yj/rustWork/rust-learning/hello-world/target/debug/hello-world: /Users/yj/rustWork/rust-learning/hello-world/src/main.rs
